e2c39cefcb6ad7e266be8846cb12a22347325d3f
Dmitry Shmanko <dimashaman@icloud.com>
2021-11-12 16:46:58 +1100

    cleaning

--- src/ContactCollection.php
+++ src/ContactCollection.php
@@ -53,14 +53,4 @@ class ContactCollection
         });
     }
 
-    private function str_lreplace($search, $replace, $subject)
-    {
-        $pos = strrpos($subject, $search);
-    
-        if ($pos !== false) {
-            $subject = substr_replace($subject, $replace, $pos, strlen($search));
-        }
-    
-        return $subject;
-    }
 }
